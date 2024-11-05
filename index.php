fb2d75bae3a334290b04609946557cff7c455b0c
EntonyDias <149632068+EntonyDias@users.noreply.github.com>
2024-11-05 19:00:58 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Pagina de MiniProjetos</title>
+   <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+   <header>
+      <h3>Projetos</h3>
+      <h3>Exercicios de fixação</h3>
+      <h3>2024/11/04</h3>
+   </header>
+   <main>
+      <div class="container">
+         <!--Lista de Atividades-->
+         <ul id="listaExercicios">
+            <li>
+               <p>Atividade</p>
+               <img src="" alt="Pagina da 1ª Atividade">
+               <a href="">Visitar</a>
+            </li>
+            <li>
+               <p>Atividade</p>
+               <img src="" alt="Pagina da 2ª Atividade">
+               <a href="">Visitar</a>
+            </li>
+            <li>
+               <p>Atividade</p>
+               <img src="" alt="Pagina da 3ª Atividade">
+               <a href="">Visitar</a>
+            </li>
+         </ul>
+         <!--Lista de Exercicio-->
+         <ul id="listaExercicio">
+            <li>
+               <p>Exercicio</p>
+               <img src="" alt="Pagina do 1º Exercicio">
+               <a href="">Visitar</a>
+            </li>
+            <li>
+               <p>Exercicio</p>
+               <img src="" alt="Pagina do 2º Exercicio">
+               <a href="">Visitar</a>
+            </li>
+            <li>
+               <p>Exercicio</p>
+               <img src="" alt="Pagina do 3º Exercicio">
+               <a href="">Visitar</a>
+            </li>
+         </ul>
+         
+         <!--Lista de Treinos-->
+         <ul id="listaTreinos">
+            <li>
+               <p>Treino</p>
+               <img src="./Treinos/images/treino1.png" alt="Pagina do 1º Treino">
+               <a href="./Treinos/treino1.php">Visitar</a>
+            </li>
+            <li>
+               <p>Treino</p>
+               <img src="" alt="Pagina do 2º Treino">
+               <a href="">Visitar</a>
+            </li>
+            <li>
+               <p>Treino</p>
+               <img src="" alt="Pagina do 3º Treino">
+               <a href="">Visitar</a>
+            </li>
+         </ul>
+      </div>
+   </main>
+   <footer>
+
+   </footer>
+</body>
+
+</html>
